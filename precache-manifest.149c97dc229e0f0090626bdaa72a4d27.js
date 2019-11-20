6aefb4f51c656627124a93001b6b7c99d3b245d9
Anatolii-Ostapenko <ostapenko.anatolii87@gmail.com>
2019-11-20 13:53:36 +0200

    Updates

---
+++ precache-manifest.149c97dc229e0f0090626bdaa72a4d27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01b1e8badbfa1235f60f035b267bab62",
+    "url": "/React_repository/index.html"
+  },
+  {
+    "revision": "03251159f5ca0c372972",
+    "url": "/React_repository/static/css/main.5ead7584.chunk.css"
+  },
+  {
+    "revision": "388b9b285b1f72e07257",
+    "url": "/React_repository/static/js/2.36960101.chunk.js"
+  },
+  {
+    "revision": "03251159f5ca0c372972",
+    "url": "/React_repository/static/js/main.526f9464.chunk.js"
+  },
+  {
+    "revision": "9c78b13127585341f97b",
+    "url": "/React_repository/static/js/runtime-main.c693fd7a.js"
+  }
+]);
