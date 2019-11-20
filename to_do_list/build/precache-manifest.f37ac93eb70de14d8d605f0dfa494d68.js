ea788ea743ce226e246e0c52c81dbbd92d7da78d
Anatolii-Ostapenko <ostapenko.anatolii87@gmail.com>
2019-11-20 13:17:23 +0200

    Add build

---
+++ to_do_list/build/precache-manifest.f37ac93eb70de14d8d605f0dfa494d68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66f0c4af38bb5ad2ea6018c3bae111e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03251159f5ca0c372972",
+    "url": "/static/css/main.5ead7584.chunk.css"
+  },
+  {
+    "revision": "388b9b285b1f72e07257",
+    "url": "/static/js/2.36960101.chunk.js"
+  },
+  {
+    "revision": "03251159f5ca0c372972",
+    "url": "/static/js/main.526f9464.chunk.js"
+  },
+  {
+    "revision": "f658dfb25e863c14f901",
+    "url": "/static/js/runtime-main.f32801d6.js"
+  }
+]);
